80ce21fb5455f19bf1fcca622283860c4a7c245d
Sam Gebhard <gebharsj@gmatc.matc.edu>
2017-04-04 15:57:43 -0500

    new level, updates to rotation

--- Assets/Game/Scripts/Gameplay Scripts/Rotate.cs
+++ Assets/Game/Scripts/Gameplay Scripts/Rotate.cs
@@ -5,13 +5,41 @@ using UnityEngine;
 public class Rotate : MonoBehaviour 
 {
 	public float rotationSpeed;
-	public bool inverted;
+
+    public enum Rotation
+    {
+       NONE,
+       NEGX, POSX,
+       NEGY, POSY,
+       NEGZ, POSZ
+    };
+
+    public Rotation myRotation = Rotation.NONE;
 
 	void Update ()
 	{
-		if (!inverted)
-			transform.Rotate (Vector3.back * rotationSpeed * Time.deltaTime);
-		else
-			transform.Rotate (Vector3.forward * rotationSpeed * Time.deltaTime);
+        switch(myRotation)
+        {
+            case Rotation.NONE:
+                break;
+            case Rotation.NEGX:
+                transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSX:
+                transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.NEGY:
+                transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSY:
+                transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.NEGZ:
+                transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSZ:
+                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+                break;
+        }
 	}
 }
