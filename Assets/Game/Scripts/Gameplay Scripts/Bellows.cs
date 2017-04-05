82520df0e869bf7bb3fe53e9e5e914d36fceb431
Sam Gebhard <gebharsj@gmatc.matc.edu>
2017-04-05 16:19:30 -0500

    added event/delegate for toggling

--- Assets/Game/Scripts/Gameplay Scripts/Bellows.cs
+++ Assets/Game/Scripts/Gameplay Scripts/Bellows.cs
@@ -6,15 +6,48 @@ public class Bellows : MonoBehaviour
 {
 	public float force;
 	public bool inverted;
+    bool isBlowing;
+    public ToggleScript toggleScript;
+
+    void OnEnable()
+    {
+        if (toggleScript != null)
+        {
+            toggleScript.OnToggle += ToggleOn;
+            toggleScript.OnExit += ToggleOff;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (toggleScript != null)
+        {
+            toggleScript.OnToggle -= ToggleOn;
+            toggleScript.OnExit -= ToggleOff;
+        }
+    }
+    
+    void ToggleOn()
+    {
+        isBlowing = true;
+    }
+
+    void ToggleOff()
+    {
+        isBlowing = false;
+    }
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.transform.tag.Equals ("Player")) 
-		{
-			if(!inverted)
-				other.GetComponent<Rigidbody>().AddForce (transform.right * force, ForceMode.Acceleration);
-			else
-				other.GetComponent<Rigidbody>().AddForce (-transform.right * force, ForceMode.Acceleration);
-		}
+        if (isBlowing)
+        {
+            if (other.transform.tag.Equals("Player"))
+            {
+                if (!inverted)
+                    other.GetComponent<Rigidbody>().AddForce(transform.right * force, ForceMode.Acceleration);
+                else
+                    other.GetComponent<Rigidbody>().AddForce(-transform.right * force, ForceMode.Acceleration);
+            }
+        }
 	}
 }
---
+++ Assets/Game/Scripts/Gameplay Scripts/ToggleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleScript : MonoBehaviour {
+
+    //public Behaviour targetObject;
+
+    public delegate void Toggle();
+    public event Toggle OnToggle;
+    public event Toggle OnExit;
+
+	void OnTriggerEnter(Collider other)
+    {
+        OnToggle();            
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        OnExit();
+    }
+}
