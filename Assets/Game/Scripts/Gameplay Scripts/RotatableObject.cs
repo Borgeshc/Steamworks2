40e84a4e584831116c51a405728d10d8bfce85cb
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-04-12 15:44:03 -0500

    Added new gears and platform

--- Assets/Game/Scripts/Gameplay Scripts/RotatableObject.cs
+++ Assets/Game/Scripts/Gameplay Scripts/RotatableObject.cs
@@ -5,29 +5,65 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class RotatableObject : MonoBehaviour 
 {
-	public float turnSpeed;
+	public float rotationSpeed;
 	public float minYRotation = -35f;
-	public float maxYRotation = 35f; // For proof of concept, basically
-	float currentRotation = 0.0f;
+	public float maxYRotation = 35f;
+	float currentRotation;
 	Quaternion yRotation;
 
 	public bool inverted;
+    public enum Rotation
+    {
+        NONE,
+        NEGX, POSX,
+        NEGY, POSY,
+        NEGZ, POSZ
+    };
+
+    public Rotation myRotation = Rotation.NONE;
+    
+    private void Start()
+    {
+        currentRotation = transform.rotation.y;
+    }
 
 	void Update()
 	{
 		#if MOBILE_INPUT
-		if(!inverted)
-		currentRotation += CrossPlatformInputManager.GetAxisRaw("Vertical") * turnSpeed * Time.deltaTime;
+		if(!inverted)rotation
+		currentRotation = CrossPlatformInputManager.GetAxisRaw("Vertical");
 		else
-		currentRotation += -CrossPlatformInputManager.GetAxisRaw("Vertical") * turnSpeed * Time.deltaTime;
+		currentRotation = -CrossPlatformInputManager.GetAxisRaw("Vertical");
 		#else
 		if(!inverted)
-			currentRotation += Input.GetAxisRaw("Vertical") * turnSpeed * Time.deltaTime;
+			currentRotation = Input.GetAxis("Vertical");
 		else
-			currentRotation += -Input.GetAxisRaw("Vertical") * turnSpeed * Time.deltaTime;
+			currentRotation = -Input.GetAxis("Vertical");
 		#endif
 
-		currentRotation = Mathf.Clamp(currentRotation, minYRotation, maxYRotation);
-		transform.rotation = Quaternion.identity * Quaternion.AngleAxis(currentRotation, transform.forward);
+		//currentRotation = Mathf.Clamp(currentRotation, minYRotation, maxYRotation);
+        switch (myRotation)
+        {
+            case Rotation.NONE:
+                break;
+            case Rotation.NEGX:
+                transform.Rotate(Vector3.left * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSX:
+                transform.Rotate(Vector3.right * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.NEGY:
+                transform.Rotate(Vector3.down * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSY:
+                transform.Rotate(Vector3.up * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.NEGZ:
+                transform.Rotate(Vector3.back * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+            case Rotation.POSZ:
+                transform.Rotate(Vector3.forward * currentRotation * rotationSpeed * Time.deltaTime);
+                break;
+        }
 	}
 }
